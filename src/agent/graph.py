0328daca369cebb18ecc924fab8696090e87e69c
Ronny Hoesada <swaption2009@gmail.com>
2024-10-06 15:03:52 -0700

    modify starter code

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -6,20 +6,20 @@ This agent returns a predefined response without using an actual LLM.
 from typing import Any, Dict
 
 from langchain_core.runnables import RunnableConfig
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START, END
 
 from agent.configuration import Configuration
 from agent.state import State
 
 
-async def my_node(state: State, config: RunnableConfig) -> Dict[str, Any]:
+async def my_custom_node(state: State, config: RunnableConfig) -> Dict[str, Any]:
     """Each node does work."""
     configuration = Configuration.from_runnable_config(config)
     # configuration = Configuration.from_runnable_config(config)
     # You can use runtime configuration to alter the behavior of your
     # graph.
     return {
-        "changeme": "output from my_node. "
+        "changeme": "output from my_custom_node. "
         f"Configured with {configuration.my_configurable_param}"
     }
 
@@ -28,11 +28,12 @@ async def my_node(state: State, config: RunnableConfig) -> Dict[str, Any]:
 workflow = StateGraph(State, config_schema=Configuration)
 
 # Add the node to the graph
-workflow.add_node("my_node", my_node)
+workflow.add_node("my_custom_node", my_custom_node)
 
 # Set the entrypoint as `call_model`
-workflow.add_edge("__start__", "my_node")
+workflow.add_edge(START, "my_custom_node")
+workflow.add_edge("my_custom_node", END)
 
 # Compile the workflow into an executable graph
 graph = workflow.compile()
-graph.name = "New Graph"  # This defines the custom name in LangSmith
+graph.name = "Ronny's Graph"  # This defines the custom name in LangSmith
